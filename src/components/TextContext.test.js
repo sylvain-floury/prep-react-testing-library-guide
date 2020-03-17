b91d4751aa6fd9f9d5d175e0bc4b511306eb38d6
Sylvain Floury <sylvain@floury.name>
2020-03-17 17:05:30 +0100

    test(context) add React Context testing

---
+++ src/components/TextContext.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {cleanup, fireEvent, render} from '@testing-library/react'
+import "@testing-library/jest-dom/extend-expect";
+import CounterProvider, {Counter, CounterContext} from './TestContext'
+
+const renderWithContext = (component) => {
+    return {
+        ...render(
+            <CounterProvider value={CounterContext}>
+                {component}
+            </CounterProvider>)
+    }
+};
+
+afterEach(cleanup);
+
+it('checks if initial state is equal to 0', () => {
+    const {getByTestId} = renderWithContext(<Counter/>);
+    expect(getByTestId('counter')).toHaveTextContent('0')
+});
+
+it('increments the counter', () => {
+    const {getByTestId} = renderWithContext(<Counter/>);
+
+    fireEvent.click(getByTestId('button-up'));
+    expect(getByTestId('counter')).toHaveTextContent('1')
+});
+
+it('decrements the counter', () => {
+    const {getByTestId} = renderWithContext(<Counter/>);
+
+    fireEvent.click(getByTestId('button-down'));
+    expect(getByTestId('counter')).toHaveTextContent('-1')
+});
