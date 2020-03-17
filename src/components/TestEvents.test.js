d0369ceb67e8e09540bc118d6ec07dd2588396d1
Sylvain Floury <sylvain@floury.name>
2020-03-17 16:04:48 +0100

    test(event) add events testing

---
+++ src/components/TestEvents.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {cleanup, fireEvent, render} from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+import TestEvents from './TestEvents'
+
+afterEach(cleanup);
+
+it('increments counter', () => {
+    const {getByTestId} = render(<TestEvents/>);
+
+    fireEvent.click(getByTestId('button-up'));
+
+    expect(getByTestId('counter')).toHaveTextContent('1')
+});
+
+it('decrements counter', () => {
+    const {getByTestId} = render(<TestEvents/>);
+
+    fireEvent.click(getByTestId('button-down'));
+
+    expect(getByTestId('counter')).toHaveTextContent('-1')
+});
+
